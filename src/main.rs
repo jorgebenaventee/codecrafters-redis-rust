86d1e93366d2d42d51e2f52dff457d61c6018241
Jorge Benavente <jorge.benavente2003@gmail.com>
2024-11-17 11:59:28 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
-#![allow(unused_imports)]
-
 use std::io::{Read, Write};
 use std::net::TcpListener;
-use std::ptr::read;
-use std::str;
+use std::thread;
 
 const PONG: &[u8] = b"+PONG\r\n";
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("Accepted new connection!");
-                let mut read_command = [0; 512];
-                loop {
-                    let result = stream.read(&mut read_command);
-                    let _ = stream.write(PONG);
+        thread::spawn(
+            || {
+                match stream {
+                    Ok(mut stream) => {
+                        println!("Accepted new connection!");
+                        let mut read_command = [0; 512];
+                        loop {
+                            let result = stream.read(&mut read_command);
+                            let _ = stream.write(PONG);
+                        }
+                    }
+                    Err(e) => {
+                        println!("error: {}", e);
+                    }
                 }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        );
     }
 }
